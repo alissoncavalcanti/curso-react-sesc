daf963804adf5be8de342ddbf222cac942da0b90
alissoncavalcanti <alissoncavalcanti1@yahoo.com.br>
2023-11-14 21:01:09 -0300

    aula 11

--- aulas/src/components/projects/project-form/index.jsx
+++ aulas/src/components/projects/project-form/index.jsx
@@ -1,19 +1,18 @@
-import dataTeams from '../../../data/data-teams.json';
-import React, { useState } from 'react'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { ptBR } from 'date-fns/locale';
-import { TextField } from '@mui/material';
+import dataTeams from "../../../data/data-teams.json";
+import { useState } from "react";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { ptBR } from "date-fns/locale";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Box, MenuItem, TextField } from "@mui/material";
 
-function ProjectForm ( {addProject} ) {
+function ProjectForm({ addProject }) {
   const [teams] = useState(dataTeams);
   const [currentTitle, setCurrentTitle] = useState("");
   const [currentDescription, setCurrentDescription] = useState("");
-  const [currentStartDate, setCurrentStartDate] = useState("");
-  const [currentDeadline, setCurrentDeadline] = useState("");
-  const [currentEndDate, setCurrentEndDate] = useState("");
+  const [currentStartDate, setCurrentStartDate] = useState(null);
+  const [currentDeadline, setCurrentDeadline] = useState(null);
+  const [currentEndDate, setCurrentEndDate] = useState(null);
   const [currentClient, setCurrentClient] = useState("");
   const [currentStatus, setCurrentStatus] = useState("");
   const [currentTeam, setCurrentTeam] = useState("");
@@ -21,115 +20,160 @@ function ProjectForm ( {addProject} ) {
   const handleSubmit = (event) => {
     event.preventDefault(); //Impede o navegador de recarregar a página
     //Validação dos campos
-    if(
-        !currentTitle || 
-        !currentDescription || 
-        !currentStartDate ||
-        !currentDeadline ||
-        !currentEndDate ||
-        !currentClient ||
-        !currentStatus ||
-        !currentTeam
+    if (
+      !currentTitle ||
+      !currentDescription ||
+      !currentStartDate ||
+      !currentEndDate ||
+      !currentClient ||
+      !currentStatus ||
+      !currentTeam
     ) {
       alert("Todos os campos são obrigatórios!");
       return;
     }
     //Adicionar uma nova tarefa à lista de tarefas
     addProject(
-        currentTitle,
-        currentDescription,
-        currentStartDate,
-        currentDeadline,
-        currentEndDate,
-        currentClient,
-        currentStatus,
-        currentTeam
+      currentTitle,
+      currentDescription,
+      currentStartDate,
+      currentDeadline,
+      currentEndDate,
+      currentClient,
+      currentStatus,
+      currentTeam
     );
     setCurrentTitle("");
     setCurrentDescription("");
-    setCurrentStartDate("");
-    setCurrentDeadline("");
-    setCurrentEndDate("");
+    setCurrentStartDate(null);
+    setCurrentDeadline(null);
+    setCurrentEndDate(null);
     setCurrentClient("");
     setCurrentTitle("");
     setCurrentStatus("");
     setCurrentTeam("");
 
     alert("Projeto cadastrado com sucesso!");
-  }
+  };
 
   return (
-    <section className='section-main'>
-      <div className='container-card'>
-        <h1>Cadastrar projeto</h1>
-        <hr />
-        <form onSubmit={handleSubmit}>
-          <label htmlFor='title'>Título</label>
-          <input
-            type='text'
-            name='title'
-            id='title'
-            placeholder='Digite o título'
-            value={currentTitle}
-            onChange={
-              (event) => 
-              setCurrentTitle(event.target.value)
-            }
-          />
-          <label htmlFor='description'>Descrição</label>
-          <textarea
-            name='description'
-            id='description'
-            placeholder='Digite a descrição do projeto'
-            value={currentDescription}
-            onChange={
-              (event) => 
-              setCurrentDescription(event.target.value)
-            }
-          />
-
-          <TextField
-            id="outlined-basic"
-            label="Cliente"
-            variant="outlined"
-            value={currentClient}
-            onChange={
-              (event) => 
-              setCurrentClient(event.target.value)
-            }
-          />
-          <br></br><br></br>
-          <label htmlFor='team'>Equipe</label>
-          <select
-            name='team'
-            id='team'
-            value={currentTeam}
-            onChange={
-              (event) => 
-                setCurrentTeam(event.target.value)
-            }
-          >
-            <option value="">Selecione uma equipe</option>
-            {teams && teams.map((team => {
-              return (
-                <React.Fragment key={team.id}>
-                  <option value={team.id}>{team.name}</option>
-                </React.Fragment>
-              )
-            }))}
-          </select>
-          <div>
-            <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDayjs}>
-              <DatePicker/>
-            </LocalizationProvider>
-          </div>
-          <button className='btn-register' type='submit'>
-            Cadastrar
-          </button>
-        </form>
+    <section className="section-main">
+      <div className="container-card">
+        <Box
+          sx={{
+            "& .MuiTextField-root": { marginY: 1},
+          }}
+        >
+          <h1>Cadastrar projeto</h1>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Título"
+              variant="outlined"
+              value={currentTitle}
+              onChange={(event) => setCurrentTitle(event.target.value)}
+            />
+            <TextField
+            fullWidth
+              name="description"
+              id="description"
+              placeholder="Digite a descrição do projeto"
+              value={currentDescription}
+              label="Descrição"
+              multiline
+              rows={6}
+              onChange={(event) => setCurrentDescription(event.target.value)}
+            />
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Cliente"
+              variant="outlined"
+              value={currentClient}
+              onChange={(event) => setCurrentClient(event.target.value)}
+            />
+            <TextField
+            fullWidth
+              select
+              fullWidth
+              label="Equipe"
+              name="team"
+              id="team"
+              value={currentTeam}
+              onChange={(event) => setCurrentTeam(event.target.value)}
+            >
+              <MenuItem value="">Selecione uma equipe</MenuItem>
+              {teams &&
+                teams.map((team) => {
+                  return (
+                    <MenuItem key={team.id} value={team.id}>
+                      {team.name}
+                    </MenuItem>
+                  );
+                })}
+            </TextField>
+            <Box sx ={{
+              display: 'flex',
+              gap: {
+                xs: '0',
+                sm: '0',
+                md: '1em',
+                lg: '1em',
+                xl: '1em'
+              },
+              flexDirection: {
+                xs: 'column',
+                sm: 'column',
+                md: 'row',
+                lg: 'row',
+                xl: 'row'
+              }
+            }}>
+              <LocalizationProvider
+                adapterLocale={ptBR}
+                dateAdapter={AdapterDateFns}
+              >
+                <DatePicker
+                  width="33%"
+                  id='startDate'
+                  label='Data de início'
+                  value={currentStartDate}
+                />
+                </LocalizationProvider>
+                <LocalizationProvider
+                adapterLocale={ptBR}
+                dateAdapter={AdapterDateFns}
+              >
+                <DatePicker
+                width="33%"
+                  id='endDate'
+                  label='Data de finalização'
+                  value={currentEndDate}
+                />
+                </LocalizationProvider>
+                <LocalizationProvider
+                adapterLocale={ptBR}
+                dateAdapter={AdapterDateFns}
+              >
+                <DatePicker 
+                width="33%"
+                  id='deadline'
+                  label='Previsão de entrega'
+                  value={currentDeadline}
+                />
+              </LocalizationProvider>
+            </Box>
+            <hr/>
+            <button className="btn-register" type="submit">
+              Cadastrar
+            </button>
+          </form>
+        </Box>
       </div>
     </section>
-  )
+  );
 }
 
 export default ProjectForm;
--- aulas/src/pages/projects/index.jsx
+++ aulas/src/pages/projects/index.jsx
@@ -1,12 +1,44 @@
-import {Header, Footer, ProjectForm} from "../../components";
+import { useState } from 'react';
+import { Footer, Header, ProjectForm, ProjectsList } from "../../components";
+
+import dataProjects from '../../data/data-projects.json';
 
 function ProjectsPage() {
-    return (
-        <>
-        <Header/>
-        <ProjectForm/>
-        <Footer/>
-        </>
-    )
+
+  const [projects, setProjects] = useState(dataProjects);
+
+  const addProject = (title, category, member) => {
+    if(!title || !category || !member) return;
+    const newProjectArray = [
+      ...projects,
+      {
+        id: Math.floor(Math.random() * 10000),
+        title,
+        category,
+        member,
+        status: "todo"
+      }
+    ];
+    setProjects(newProjectArray);
+  }
+
+  const deleteProject = (id) => {
+    const newProjects = [...projects];
+    const filteredProjects = newProjects.filter(project => project.id !== id ? project : null);
+    setProjects(filteredProjects);
+  }
+
+  return (
+    <>
+      <Header />
+      <ProjectForm addProject={addProject} />
+      <ProjectsList
+        projects={projects}
+        deleteProject={deleteProject}
+      />
+      <Footer />
+    </>
+  )
 }
+
 export default ProjectsPage;
