f598c4fc62cbaf71f81928624ad4ad4d4f0f344d
alissoncavalcanti <alissoncavalcanti1@yahoo.com.br>
2023-12-12 20:59:36 -0300

    Aula 14

--- aulas/src/components/tasks/tasks-list/index.jsx
+++ aulas/src/components/tasks/tasks-list/index.jsx
@@ -1,34 +1,33 @@
-import Task from '../task';
-import './style.css'
+import Task from "../task";
+import './style.css';
 
-function TasksList({tasks, startTask, closeTask, deleteTask}) {
-    return (
-        <section className='section-main'>
-        <div className='container-card'>
-          <h1>Lista de tarefas</h1>
-          <hr />
-
-          {/*INÍCIO DA TASK */}
-          {tasks && tasks.map((task) => {
-            return(
-              <Task 
-                key ={task.id}
-                id={task.id}
-                title={task.title}
-                member={task.member}
-                category={task.category}
-                idProject={task.idProject}
-                status={task.status}
-                startTask={startTask}
-                closeTask={closeTask}
-                deleteTask={deleteTask}
-                />
-            )
-          })}
-          {/*FIM DA TASK */}
-        </div>
-        </section>
-    )
+function TasksList( {tasks, startTask, closeTask, deleteTask} ) {
+  return (
+    <section className='section-main'>
+      <div className='container-card'>
+        <h1>Lista de tarefas</h1>
+        <hr />
+        {/* INÍCIO DA TASK */}
+        {tasks && tasks.map((task) => {
+          return(
+            <Task
+              key={task.id}
+              id={task.id}
+              title={task.attributes.title}
+              idMember={task.attributes.member.data.id}
+              idCategory={task.attributes.category.data.id}
+              idProject={task.attributes.project.data.id}
+              status={task.attributes.status}
+              startTask={startTask}
+              closeTask={closeTask}
+              deleteTask={deleteTask}
+            />
+          )
+        })}
+        {/* FIM DA TASK */}
+      </div>
+    </section>
+  )
 }
 
 export default TasksList;
--- aulas/src/context/project-context/index.jsx
+++ aulas/src/context/project-context/index.jsx
@@ -1,51 +1,72 @@
-import { createContext, useState } from "react"
-import dataProjects from "../../data/data-projects.json"
-import { format } from "date-fns";
+import { createContext, useState, useEffect } from "react"
+import { format } from 'date-fns'
+//import dataProjects from "../../data/data-projects.json"
+import { projectsApi } from "../../api/projects";
+import { teamsApi } from "../../api/teams";
 
 export const ProjectContext = createContext({});
 
 export const ProjectProvider = ({children}) => {
-    const [projects, setProjects] = useState(dataProjects);
+  const [projects, setProjects] = useState(null);
+  const [teams, setTeams] = useState(null);
 
-    const addProject = (
-      title,
-      description,
-      startDate,
-      deadline,
-      endDate,
-      client,
-      status,
-      idTeam
-    ) => {
-      if(!title || !description || !deadline || !client || !status || !idTeam) return;
-      const newProjectsArray = [
-        ...projects,
-        {
-          id: Math.floor(Math.random() * 10000),
-          title,
-          description,
-          startDate: startDate ? format(startDate, 'yyyy/MM/dd') : '',
-          deadline: deadline ? format(deadline, 'yyyy/MM/dd') : '',
-          endDate: endDate ? format(endDate, 'yyyy/MM/dd') : '',
-          client,
-          idTeam,
-          status: "todo"
-        }
-      ];
-  
-      console.log('newProjectsArray', newProjectsArray);
-      setProjects(newProjectsArray);
-    }
-  
-    const deleteProject = (id) => {
-      const newProjects = [...projects];
-      const filteredProjects = newProjects.filter(project => project.id !== id ? project : null);
-      setProjects(filteredProjects);
-    }
+  const addProject = (
+    title,
+    description,
+    startDate,
+    deadline,
+    endDate,
+    client,
+    status,
+    idTeam
+  ) => {
+    if(!title || !description || !deadline || !client || !status || !idTeam) return;
+    const newProjectsArray = [
+      ...projects,
+      {
+        id: Math.floor(Math.random() * 10000),
+        title,
+        description,
+        startDate: startDate ? format(startDate, 'yyyy/MM/dd') : '',
+        deadline: deadline ? format(deadline, 'yyyy/MM/dd') : '',
+        endDate: endDate ? format(endDate, 'yyyy/MM/dd') : '',
+        client,
+        idTeam,
+        status: "todo"
+      }
+    ];
+    setProjects(newProjectsArray);
+  }
 
-    return (
-        <ProjectContext.Provider value={{ projects, addProject, deleteProject }}>
-          {children}
-        </ProjectContext.Provider>
-      );
+  const deleteProject = (id) => {
+    const newProjects = [...projects];
+    const filteredProjects = newProjects.filter(project => project.id !== id ? project : null);
+    setProjects(filteredProjects);
+  }
+
+  useEffect(() => {
+    const fetchDataProjects = async () => {
+      const result = await projectsApi.getProjects();
+      setProjects(result);
+    }
+    if(projects === null) {
+      fetchDataProjects();
+    }
+  },[projects]);
+
+  useEffect(() => {
+    const fetchDataTeams = async () => {
+      const result = await teamsApi.getTeams();
+      setTeams(result);
+    }
+    if(teams === null) {
+      fetchDataTeams();
+    }
+  },[teams]);
+
+  return(
+    <ProjectContext.Provider value={{ projects, teams, addProject, deleteProject }}>
+      {children}
+    </ProjectContext.Provider>
+  );
 }
